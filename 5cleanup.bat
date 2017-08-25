8248e37f04d558da4d7bd905979fad3bcf917de0
Sentinel <djdistorted13@gmail.com>
2017-08-24 23:25:58 -0400

    commit

--- 5cleanup.bat
+++ 5cleanup.bat
@@ -5,4 +5,3 @@ mkdir temp
 echo WE ALL DONE FAM >> "C:\Users\jadmin\Documents\mcbackups\logs\backup.log"
 echo The operation was completed successfully on %date% %time% >> "C:\Users\jadmin\Documents\mcbackups\logs\backup.log"
 cd "C:\Users\jadmin\Documents\mcbackups\util"
-asldfjaslkfjalskdfklsjdf
